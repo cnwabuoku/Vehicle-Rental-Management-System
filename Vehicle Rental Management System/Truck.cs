327d6086522edb66b6e8ec8eb2a9c4330d19f604
cnwabuoku <nwabuokujohn@gmail.com>
2024-07-13 15:17:16 -0400

    created truck class

---
+++ Vehicle Rental Management System/Truck.cs
@@ -0,0 +1,33 @@
+using System;
+ 
+namespace VehicleRentalManagementSystem
+{
+
+public class Truck : Vehicle
+{
+    public int Capacity { get; set; }
+    public string TruckType { get; set; }
+    public bool FourWhealDrive { get; set; }
+
+    public Truck(string model, string manufacturer, int year, decimal rentalPrice, int capacity, string truckType, bool fourWheelDrive)
+        : base(model, manufacturer, year, rentalPrice)
+        {
+            Capacity = capacity;
+            TruckType = truckType;
+            FourWhealDrive = fourWheelDrive;
+        }
+        public override void DisplayDetails()
+        {
+            Console.WriteLine("Truck Details:");
+            Console.WriteLine("Model: {0}", Model);
+            Console.WriteLine("Manufacturer: {0}", Manufacturer);
+            Console.WriteLine("Year: {0}", Year);
+            Console.WriteLine("Rental Price: {0}", RentalPrice);
+            Console.WriteLine("Seats: {0}", Capacity);
+            Console.WriteLine("Engine Type: {0}", TruckType);
+            Console.WriteLine("Transmission: {0}", FourWhealDrive);
+            Console.WriteLine("");
+        }
+
+    }
+}
