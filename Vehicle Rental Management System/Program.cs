a3a608686102cb2e75c42b54e5a69ce929645404
cnwabuoku <nwabuokujohn@gmail.com>
2024-07-17 12:59:04 -0400

    Developing functionality in the Program class

--- Vehicle Rental Management System/Program.cs
+++ Vehicle Rental Management System/Program.cs
@@ -1,10 +1,16 @@
 ﻿using System;
  
- namespace VehicleRental
+namespace VehicleRentalManagementSystem
+{
+
+ public class Program
  {
-
-
-
-
+    public static void Main(string[] args)
+    {
+        Car car = new Car("Model S", "Tesla", 2020, 99.99m, 5, "Electric", "Automatic", true);
+        car.DisplayDetails();
+    }
 
  }
+
+}
--- Vehicle Rental Management System/obj/Debug/net8.0/Vehicle Rental Management System.AssemblyInfo.cs
+++ Vehicle Rental Management System/obj/Debug/net8.0/Vehicle Rental Management System.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vehicle Rental Management System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b4f73e10a8e89d649e420dec828c5fd36876167")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5402f90f2ec5f3b5430aae88cc0f17d39b8f7e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vehicle Rental Management System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vehicle Rental Management System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
